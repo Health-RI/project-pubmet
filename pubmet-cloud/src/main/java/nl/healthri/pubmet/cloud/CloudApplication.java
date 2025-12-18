87594d091be1579555ce3b6292d6be11845cbcdf
kburger <burger+github@gmail.com>
2025-12-18 13:30:12 +0100

    feat: add stub cloud app

---
+++ pubmet-cloud/src/main/java/nl/healthri/pubmet/cloud/CloudApplication.java
@@ -0,0 +1,16 @@
+/*
+ * SPDX-FileCopyrightText: 2025 Health-RI
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package nl.healthri.pubmet.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CloudApplication.class, args);
+    }
+}
