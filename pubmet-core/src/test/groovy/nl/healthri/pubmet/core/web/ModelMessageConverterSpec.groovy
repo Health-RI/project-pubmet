88c9a69ca57624f00e2f2ea80a90b1e9636b7563
kburger <burger+github@gmail.com>
2025-12-22 14:15:02 +0100

    feat: add test scaffolding

---
+++ pubmet-core/src/test/groovy/nl/healthri/pubmet/core/web/ModelMessageConverterSpec.groovy
@@ -0,0 +1,12 @@
+/*
+ * SPDX-FileCopyrightText: 2025 Health-RI
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package nl.healthri.pubmet.core.web
+
+import spock.lang.Specification
+
+class ModelMessageConverterSpec extends Specification {
+    // TODO implement me
+}
