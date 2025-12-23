c5f41f9bbd0d9d6346306df3b25ce551ad304c3f
SeanBerrieHRI <sean.berrie@health-ri.nl>
2025-12-23 15:15:06 +0100

    test: add junit test for writing valid model to response body

--- pubmet-core/src/test/java/nl/healthri/pubmet/core/web/ModelMessageConverterTest.java
+++ pubmet-core/src/test/java/nl/healthri/pubmet/core/web/ModelMessageConverterTest.java
@@ -5,6 +5,48 @@
  */
 package nl.healthri.pubmet.core.web;
 
-public class ModelMessageConverterTest {
-    // TODO implement me
+import org.eclipse.rdf4j.model.Model;
+import org.eclipse.rdf4j.model.impl.LinkedHashModel;
+import org.eclipse.rdf4j.model.util.Values;
+import org.eclipse.rdf4j.model.vocabulary.RDFS;
+import org.eclipse.rdf4j.rio.RDFFormat;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.mock.http.MockHttpOutputMessage;
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+class ModelMessageConverterTest {
+
+    private ModelMessageConverter converter;
+
+    @BeforeEach
+    void setUp() {
+        converter = new ModelMessageConverter(RDFFormat.TURTLE);
+    }
+
+    private Model createSampleModel() {
+        var model = new LinkedHashModel();
+        var subject = Values.iri("http://example.com");
+        model.add(subject, RDFS.LABEL, Values.literal("hello world"));
+
+        return model;
+    }
+
+    @Test
+    void GivenValidModel_WhenWriteInternal_ReturnModelContentAsBody() throws IOException {
+        // Arrange
+        var expected = "\r\n<http://example.com> <http://www.w3.org/2000/01/rdf-schema#label> \"hello world\" .\r\n";
+        var model = createSampleModel();
+        var message = new MockHttpOutputMessage();
+
+        // Act
+        converter.writeInternal(model, message);
+
+        // Assert
+        var actual = message.getBodyAsString();
+        assertEquals(expected, actual);
+    }
 }
