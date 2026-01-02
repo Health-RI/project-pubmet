0fc6ddad0199b69cd6587e3bae7d832fc139c259
SeanBerrieHRI <sean.berrie@health-ri.nl>
2026-01-02 15:39:33 +0100

    docs: add license headers

--- pubmet-core/src/main/java/nl/healthri/pubmet/core/web/controller/IndexController.java
+++ pubmet-core/src/main/java/nl/healthri/pubmet/core/web/controller/IndexController.java
@@ -1,3 +1,8 @@
+/*
+ * SPDX-FileCopyrightText: 2025 Health-RI
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
 package nl.healthri.pubmet.core.web.controller;
 
 import nl.healthri.pubmet.core.domain.Index;
