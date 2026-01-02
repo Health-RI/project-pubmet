c681149008916903afec48e80c78b64bf210d7a8
SeanBerrieHRI <sean.berrie@health-ri.nl>
2026-01-02 15:35:05 +0100

    feat: add index service with in memory storage to create and find index.

---
+++ pubmet-core/src/main/java/nl/healthri/pubmet/core/services/IndexService.java
@@ -0,0 +1,28 @@
+package nl.healthri.pubmet.core.services;
+
+import nl.healthri.pubmet.core.domain.Index;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import java.util.*;
+
+@Service
+public class IndexService {
+    private static final Logger logger = LoggerFactory.getLogger(IndexService.class);
+
+    public final Map<UUID, Index> inMemoryIndexes = new HashMap<>();
+
+    public Index create(Index index) {
+        inMemoryIndexes.put(index.id, index);
+        return index;
+    }
+
+    public Index findById(UUID id) {
+        Index found = inMemoryIndexes.get(id);
+        if (found == null) {
+            throw new NoSuchElementException("Index not found with ID: " + id);
+        }
+        return found;
+    }
+}
