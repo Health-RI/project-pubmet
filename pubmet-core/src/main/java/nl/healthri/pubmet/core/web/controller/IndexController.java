5f5ad63f8a67c5c6a28112537e6310c0d148ab95
SeanBerrieHRI <sean.berrie@health-ri.nl>
2026-01-02 15:36:10 +0100

    feat: add index controller to create and find index.

---
+++ pubmet-core/src/main/java/nl/healthri/pubmet/core/web/controller/IndexController.java
@@ -0,0 +1,28 @@
+package nl.healthri.pubmet.core.web.controller;
+
+import nl.healthri.pubmet.core.domain.Index;
+import nl.healthri.pubmet.core.services.IndexService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping(path = "index")
+public class IndexController {
+
+    private final IndexService indexService;
+
+    public IndexController(IndexService indexService) {
+        this.indexService = indexService;
+    }
+
+    @PostMapping
+    public Index create(@RequestBody Index index) {
+        return indexService.create(index);
+    }
+
+    @GetMapping("/{id}")
+    public Index GetById(@PathVariable UUID id) {
+        return indexService.findById(id);
+    }
+}
