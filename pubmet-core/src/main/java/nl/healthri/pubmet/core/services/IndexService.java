f618d5a9e8cd917f901a0b1d8f416c55c0e297a8
SeanBerrieHRI <sean.berrie@health-ri.nl>
2026-01-06 10:32:46 +0100

    test: add index service tests

--- pubmet-core/src/main/java/nl/healthri/pubmet/core/services/IndexService.java
+++ pubmet-core/src/main/java/nl/healthri/pubmet/core/services/IndexService.java
@@ -19,6 +19,8 @@ public class IndexService {
 
     public Index create(Index index) {
         inMemoryIndexes.put(index.id, index);
+        logger.info("Successfully uploaded indexes. Total models: {}", inMemoryIndexes.size());
+
         return index;
     }
 
@@ -27,6 +29,7 @@ public class IndexService {
         if (found == null) {
             throw new NoSuchElementException("Index not found with ID: " + id);
         }
+
         return found;
     }
 }
