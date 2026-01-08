739f9e8804b70c60871a6bca20bfc22f421060ad
kburger <burger+github@gmail.com>
2026-01-08 13:22:59 +0100

    feat: add harvester api stub

---
+++ pubmet-cloud/src/main/java/nl/healthri/pubmet/cloud/api/Harvester.java
@@ -0,0 +1,14 @@
+/*
+ * SPDX-FileCopyrightText: 2025 Health-RI
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package nl.healthri.pubmet.cloud.api;
+
+import java.net.URL;
+
+// TODO consider moving to a separate module
+public interface Harvester {
+    // TODO refine the method signature
+    void harvest(URL endpoint);
+}
